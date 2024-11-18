68a16c354273c282d1de7bb377d7440d52eff923
Devin Patel <116824449+devinpatel26@users.noreply.github.com>
2024-11-18 20:06:50 +0530

    Lesson_model completed and cloudinary public id prefix configured for course model.

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from cloudinary import CloudinaryImage
-from .models import Course
+from .models import Course , Lesson
 from django.utils.html import mark_safe, format_html
 
 
 # Register your models here.
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 1
+    readonly_fields = ['updated']
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]   
     list_display = ['title', 'status', 'access']
     fields = ['title', 'description', 'status', 'image', 'access', 'display_image', 'is_published']
     list_filter = ['status', 'access']
@@ -18,4 +24,4 @@ class CourseAdmin(admin.ModelAdmin):
         # Accessing the image attribute of the Course instance
         url = obj.image_admin
         return format_html(f'<img src="{url}"/>')
-
+    
--- src/courses/models.py
+++ src/courses/models.py
@@ -1,5 +1,6 @@
 import helpers
 from django.db import models
+from django.utils.text import slugify
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
@@ -18,13 +19,37 @@ class PublishedStatus(models.TextChoices):
 def handle_upload(instance, filename):
     return f'courses/images/{filename}'
 
+
+def get_public_id_prefix(instance ,*args, **kwargs):
+    print(args, kwargs)
+    title = instance.title
+    if title:
+        slug = slugify(title)
+        return f'course/{slug}'
+    if instance.id:
+        return f'course/{instance.id}'
+    return 'course/unknown'
+
+def get_public_id_prefix(instance ,*args, **kwargs):
+    print(args, kwargs)
+    title = instance.title
+    if title:
+        slug = slugify(title)
+        return f'course/{slug}'
+    if instance.id:
+        return f'course/{instance.id}'
+    return 'course/unknown'
+    
+
+
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     # image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
-    image = CloudinaryField('image', blank=True, null=True)
+    image = CloudinaryField('image', blank=True, null=True , public_id_prefix=get_public_id_prefix)
     access = models.CharField(max_length=5, choices=AccessChoices.choices, default=AccessChoices.EMAIL_REQUIRED)
     status = models.CharField(max_length=20, choices=PublishedStatus.choices, default=PublishedStatus.DRAFT)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
     @property
@@ -52,8 +77,8 @@ class Course(models.Model):
             'crop': crop
         }
         if as_html:
-            return self.image.image(**image_option)
-        # CloudinaryImage(str(obj.image)).image(width = 150, crop = "pad")   
+            # CloudinaryImage(str(obj.image)).image(width = 150, crop = "pad")  
+            return self.image.image(**image_option) 
         url = self.image.build_url(**image_option)
         return url
 """
@@ -74,8 +99,21 @@ class Course(models.Model):
 # course_obj.lesson_set.first()
 # Lesson.objects.filter(course__id=course_obj.id)
 # course_qs = Course.objects.filter(id=course_obj.id)
+# lesson_obj = Lesson.objects.first()
+# new_course_obj = lesson_obj.course 
+# new_course_lesson = new_course_obj.lesson_set.all()
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField('thumbnail', blank=True, null=True)
+    video = CloudinaryField('video', blank=True, null=True , resource_type='video')
+    order = models.IntegerField(default=0)
+    can_preview = models.BooleanField(default=False , help_text='If user does not access to course can preview this lesson')
+    status = models.CharField(max_length=20, choices=PublishedStatus.choices, default=PublishedStatus.PUBLISHED)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', '-updated']
